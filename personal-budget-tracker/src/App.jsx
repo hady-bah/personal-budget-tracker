0270be69861e4d9b2f3384ba44a149e6fb723cd9
Hady Bah <hadymbah@gmail.com>
2023-06-25 14:39:50 -0400

    iteration 1 complete

--- personal-budget-tracker/src/App.jsx
+++ personal-budget-tracker/src/App.jsx
@@ -8,12 +8,19 @@ function App() {
 
   return (
     <>
-      <a href="">
+    <div className = "header">
+      <a href="https://github.com/hady-bah/personal-budget-tracker/blob/main/README.md">
         <img className="logo moneyway" src="https://static.coinpaprika.com/coin/smcn-safeminecoin/logo.png?rev=10773571"  alt="MoneyWay logo" />
       </a>
-      
-      <TransactionsList transactions={transactions[0]}/>
-      
+    </div>
+    
+    <div className = "sidebar">
+      {/* map function to iterate trhough the array and display transactions */}
+      {transactions.map((transaction, index) => (
+        <TransactionsList key={index} transactions={transaction} />
+      ))}
+    </div>
+
     </>
   )
 }
